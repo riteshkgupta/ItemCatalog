c0cb14a967feb2192de397e2c6249e27fb17b9bf
riteshkgupta <techie.riteshkgupta@gmail.com>
2020-01-16 11:33:33 +0530

    commit

---
+++ src/main/java/com/app/itemcatalog/ItemCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.app.itemcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemCatalogApplication.class, args);
+	}
+
+}
